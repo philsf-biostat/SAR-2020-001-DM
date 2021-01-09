18d33460c52924b75c28e53c74608c6d638b81ff
Felipe Figueiredo <philsf79@gmail.com>
2021-01-09 18:16:11 -0300

    novas variáveis criadas: EVD e DOMINANTE

--- scripts/input.R
+++ scripts/input.R
@@ -2,3 +2,11 @@ library(readxl)
 library(data.table)
 
 dados <- data.table(read_excel("dataset/basedados_DM_2020.xlsx"))
+
+# novas variáveis ---------------------------------------------------------
+
+# EVD = diferença entre EVD pós e pré
+dados$EVD <- dados$`EVD PÓS` - dados$`EVD PRÉ`
+
+# Lado acometido é o lado dominante?
+dados[, DOMINANTE := `LADO ACOMETIDO` == `LADO DOMINANTE`]
