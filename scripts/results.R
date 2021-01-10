fb22b141e2483d7f72f77275992b37d7afb4d94b
Felipe Figueiredo <philsf79@gmail.com>
2021-01-09 23:05:00 -0300

    manter as rownames da tabela 1

--- scripts/results.R
+++ scripts/results.R
@@ -13,8 +13,10 @@ tab1.raw.sex <- CreateTableOne(vars = "SEXO",  data = dados)
 tab1.sex <- print(tab1.raw.sex, exact = TRUE, printToggle = FALSE, showAllLevels = TRUE)
 
 # juntar as tabelas
+tab1.names <- c(rownames(tab1.sex), rownames(tab1)[2:8]) # salvar rownames
 tab1 <- cbind(level = rep("", 7), tab1[2:8]) # criar espaço para juntar os níveis de SEXO
 tab1 <- rbind(tab1.sex, tab1) # juntar as tabelas
+rownames(tab1) <- tab1.names # restaurar rownames
 
 # tabela 2 - avaliação das vias cirúrgicas --------------------------------
 
