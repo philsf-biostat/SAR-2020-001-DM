0a724bb8ecc6679416fd5fcf8c0fdf27a8403742
Felipe Figueiredo <philsf79@gmail.com>
2021-01-09 22:54:08 -0300

    tabela 1 agora exibe os dois níveis de SEXO + simplifica a exibição de outras variáveis
    a opção dropEqual é nova, e muito útil

--- scripts/results.R
+++ scripts/results.R
@@ -4,8 +4,17 @@ library(tableone)
 
 # tabela 1 - características dos participantes ----------------------------
 
-tab1.raw <- CreateTableOne(vars = covars,  data = dados)
-tab1 <- print(tab1.raw, exact = TRUE, printToggle = FALSE)
+# separar sexo da tab1
+tab1.raw <- CreateTableOne(vars = setdiff(covars, "SEXO"),  data = dados)
+tab1 <- print(tab1.raw, exact = TRUE, printToggle = FALSE, dropEqual = TRUE)
+
+# sexo será exibido com ambos os níveis
+tab1.raw.sex <- CreateTableOne(vars = "SEXO",  data = dados)
+tab1.sex <- print(tab1.raw.sex, exact = TRUE, printToggle = FALSE, showAllLevels = TRUE)
+
+# juntar as tabelas
+tab1 <- cbind(level = rep("", 7), tab1[2:8]) # criar espaço para juntar os níveis de SEXO
+tab1 <- rbind(tab1.sex, tab1) # juntar as tabelas
 
 # tabela 2 - avaliação das vias cirúrgicas --------------------------------
 
